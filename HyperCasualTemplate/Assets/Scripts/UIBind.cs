4bc5ea22e8d5037c82152fa77219ed9c8aba58ce
Daybson Paisante <daybson.paisante@outlook.com>
2019-12-05 12:51:23 -0300

    + Cena inicial com esboço da UI + fonte 18-vag-rounded-... + TextMeshPro

---
+++ HyperCasualTemplate/Assets/Scripts/UIBind.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIBind : MonoBehaviour
+{
+    public TextMeshProUGUI LevelCount;
+    public TextMeshProUGUI ProgressCount;
+    public Slider ProgressSlider;
+    public TextMeshProUGUI Message;
+}
